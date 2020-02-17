f4e0f8b966f15539ca8a6012ddadbd03a4c7930e
httpdigest <kburjack@googlemail.com>
2020-02-17 20:55:12 +0100

    Simplify arcball camera demo

--- src/org/joml/lwjgl/ArcBallCameraDemo.java
+++ src/org/joml/lwjgl/ArcBallCameraDemo.java
@@ -8,161 +8,60 @@ import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
 
 import org.joml.Matrix4f;
-import org.joml.camera.ArcBallCamera;
+import org.joml.Vector3f;
 import org.lwjgl.BufferUtils;
-import org.lwjgl.glfw.*;
 import org.lwjgl.opengl.*;
 
+/**
+ * Simple arcball camera demo.
+ * 
+ * @author Kai Burjack
+ */
 public class ArcBallCameraDemo {
-    GLFWErrorCallback errorCallback;
-    GLFWKeyCallback keyCallback;
-    GLFWFramebufferSizeCallback fbCallback;
-    GLFWCursorPosCallback cpCallback;
-    GLFWScrollCallback sCallback;
-    GLFWMouseButtonCallback mbCallback;
+    private static long window;
+    private static int width = 800;
+    private static int height = 600;
+    private static float zoom = 20;
+    private static int mouseX, mouseY;
+    private static final Vector3f center = new Vector3f();
+    private static float pitch = 0.3f, yaw = 0.2f;
 
-    long window;
-    int width = 800;
-    int height = 600;
-    int x, y;
-    float zoom = 20;
-    int mouseX, mouseY;
-    boolean down;
-
-    void run() {
-        try {
-            init();
-            loop();
-
-            glfwDestroyWindow(window);
-            keyCallback.free();
-            fbCallback.free();
-            cpCallback.free();
-            sCallback.free();
-            mbCallback.free();
-        } finally {
-            glfwTerminate();
-            errorCallback.free();
-        }
-    }
-
-    ArcBallCamera cam = new ArcBallCamera();
-
-    void init() {
-        glfwSetErrorCallback(errorCallback = GLFWErrorCallback.createPrint(System.err));
-        if (!glfwInit())
-            throw new IllegalStateException("Unable to initialize GLFW");
-
-        // Configure our window
-        glfwDefaultWindowHints();
-        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
-        glfwWindowHint(GLFW_SAMPLES, 4);
-
-        window = glfwCreateWindow(width, height, "Hello ArcBall Camera!", NULL, NULL);
-        if (window == NULL)
-            throw new RuntimeException("Failed to create the GLFW window");
-
-        System.out.println("Press ENTER to randomly reposition the cube on the grid.");
-        glfwSetKeyCallback(window, keyCallback = new GLFWKeyCallback() {
-            @Override
-            public void invoke(long window, int key, int scancode, int action, int mods) {
-                if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
-                    glfwSetWindowShouldClose(window, true);
-
-                if (key == GLFW_KEY_ENTER && action == GLFW_PRESS) {
-                    cam.center((float) Math.random() * 20.0f - 10.0f, 0.0f, (float) Math.random() * 20.0f - 10.0f);
-                }
-            }
-        });
-        glfwSetFramebufferSizeCallback(window, fbCallback = new GLFWFramebufferSizeCallback() {
-            @Override
-            public void invoke(long window, int w, int h) {
-                if (w > 0 && h > 0) {
-                    width = w;
-                    height = h;
-                }
-            }
-        });
-        glfwSetCursorPosCallback(window, cpCallback = new GLFWCursorPosCallback() {
-            @Override
-            public void invoke(long window, double xpos, double ypos) {
-                x = (int) xpos - width / 2;
-                y = height / 2 - (int) ypos;
-            }
-        });
-        glfwSetMouseButtonCallback(window, mbCallback = new GLFWMouseButtonCallback() {
-            @Override
-            public void invoke(long window, int button, int action, int mods) {
-                if (action == GLFW_PRESS) {
-                    down = true;
-                    mouseX = x;
-                    mouseY = y;
-                } else if (action == GLFW_RELEASE) {
-                    down = false;
-                }
-            }
-        });
-        glfwSetScrollCallback(window, sCallback = new GLFWScrollCallback() {
-            @Override
-            public void invoke(long window, double xoffset, double yoffset) {
-                if (yoffset > 0) {
-                    zoom /= 1.1f;
-                } else {
-                    zoom *= 1.1f;
-                }
-            }
-        });
-
-        GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-        glfwSetWindowPos(window, (vidmode.width() - width) / 2, (vidmode.height() - height) / 2);
-
-        IntBuffer framebufferSize = BufferUtils.createIntBuffer(2);
-        nglfwGetFramebufferSize(window, memAddress(framebufferSize), memAddress(framebufferSize) + 4);
-        width = framebufferSize.get(0);
-        height = framebufferSize.get(1);
-
-        glfwMakeContextCurrent(window);
-        glfwSwapInterval(0);
-        glfwShowWindow(window);
-    }
-
-    void renderCube() {
+    private static void renderCube() {
         glBegin(GL_QUADS);
-        glColor3f(   0.0f,  0.0f,  0.2f );
-        glVertex3f(  0.5f, -0.5f, -0.5f );
-        glVertex3f(  0.5f,  0.5f, -0.5f );
-        glVertex3f( -0.5f,  0.5f, -0.5f );
-        glVertex3f( -0.5f, -0.5f, -0.5f );
-        glColor3f(   0.0f,  0.0f,  1.0f );
-        glVertex3f(  0.5f, -0.5f,  0.5f );
-        glVertex3f(  0.5f,  0.5f,  0.5f );
-        glVertex3f( -0.5f,  0.5f,  0.5f );
-        glVertex3f( -0.5f, -0.5f,  0.5f );
-        glColor3f(   1.0f,  0.0f,  0.0f );
-        glVertex3f(  0.5f, -0.5f, -0.5f );
-        glVertex3f(  0.5f,  0.5f, -0.5f );
-        glVertex3f(  0.5f,  0.5f,  0.5f );
-        glVertex3f(  0.5f, -0.5f,  0.5f );
-        glColor3f(   0.2f,  0.0f,  0.0f );
-        glVertex3f( -0.5f, -0.5f,  0.5f );
-        glVertex3f( -0.5f,  0.5f,  0.5f );
-        glVertex3f( -0.5f,  0.5f, -0.5f );
-        glVertex3f( -0.5f, -0.5f, -0.5f );
-        glColor3f(   0.0f,  1.0f,  0.0f );
-        glVertex3f(  0.5f,  0.5f,  0.5f );
-        glVertex3f(  0.5f,  0.5f, -0.5f );
-        glVertex3f( -0.5f,  0.5f, -0.5f );
-        glVertex3f( -0.5f,  0.5f,  0.5f );
-        glColor3f(   0.0f,  0.2f,  0.0f );
-        glVertex3f(  0.5f, -0.5f, -0.5f );
-        glVertex3f(  0.5f, -0.5f,  0.5f );
-        glVertex3f( -0.5f, -0.5f,  0.5f );
-        glVertex3f( -0.5f, -0.5f, -0.5f );
+        glColor3f(0.0f, 0.0f, 0.2f);
+        glVertex3f(0.5f, -0.5f, -0.5f);
+        glVertex3f(0.5f, 0.5f, -0.5f);
+        glVertex3f(-0.5f, 0.5f, -0.5f);
+        glVertex3f(-0.5f, -0.5f, -0.5f);
+        glColor3f(0.0f, 0.0f, 1.0f);
+        glVertex3f(0.5f, -0.5f, 0.5f);
+        glVertex3f(0.5f, 0.5f, 0.5f);
+        glVertex3f(-0.5f, 0.5f, 0.5f);
+        glVertex3f(-0.5f, -0.5f, 0.5f);
+        glColor3f(1.0f, 0.0f, 0.0f);
+        glVertex3f(0.5f, -0.5f, -0.5f);
+        glVertex3f(0.5f, 0.5f, -0.5f);
+        glVertex3f(0.5f, 0.5f, 0.5f);
+        glVertex3f(0.5f, -0.5f, 0.5f);
+        glColor3f(0.2f, 0.0f, 0.0f);
+        glVertex3f(-0.5f, -0.5f, 0.5f);
+        glVertex3f(-0.5f, 0.5f, 0.5f);
+        glVertex3f(-0.5f, 0.5f, -0.5f);
+        glVertex3f(-0.5f, -0.5f, -0.5f);
+        glColor3f(0.0f, 1.0f, 0.0f);
+        glVertex3f(0.5f, 0.5f, 0.5f);
+        glVertex3f(0.5f, 0.5f, -0.5f);
+        glVertex3f(-0.5f, 0.5f, -0.5f);
+        glVertex3f(-0.5f, 0.5f, 0.5f);
+        glColor3f(0.0f, 0.2f, 0.0f);
+        glVertex3f(0.5f, -0.5f, -0.5f);
+        glVertex3f(0.5f, -0.5f, 0.5f);
+        glVertex3f(-0.5f, -0.5f, 0.5f);
+        glVertex3f(-0.5f, -0.5f, -0.5f);
         glEnd();
     }
 
-    void renderGrid() {
+    private static void renderGrid() {
         glBegin(GL_LINES);
         glColor3f(0.2f, 0.2f, 0.2f);
         for (int i = -20; i <= 20; i++) {
@@ -174,70 +73,75 @@ public class ArcBallCameraDemo {
         glEnd();
     }
 
-    void loop() {
-        GL.createCapabilities();
-
-        // Set the clear color
-        glClearColor(0.9f, 0.9f, 0.9f, 1.0f);
-        // Enable depth testing
-        glEnable(GL_DEPTH_TEST);
-        glLineWidth(1.4f);
-
-        // Remember the current time.
-        long lastTime = System.nanoTime();
-
-        Matrix4f mat = new Matrix4f();
-        // FloatBuffer for transferring matrices to OpenGL
-        FloatBuffer fb = BufferUtils.createFloatBuffer(16);
-
-        cam.setAlpha((float) Math.toRadians(-20));
-        cam.setBeta((float) Math.toRadians(20));
-
-        while (!glfwWindowShouldClose(window)) {
-            /* Set input values for the camera */
-            if (down) {
-                cam.setAlpha(cam.getAlpha() + Math.toRadians((x - mouseX) * 0.1f));
-                cam.setBeta(cam.getBeta() + Math.toRadians((mouseY - y) * 0.1f));
-                mouseX = x;
-                mouseY = y;
+    public static void main(String[] args) {
+        if (!glfwInit())
+            throw new IllegalStateException("Unable to initialize GLFW");
+        window = glfwCreateWindow(width, height, "Hello ArcBall Camera!", NULL, NULL);
+        if (window == NULL)
+            throw new RuntimeException("Failed to create the GLFW window");
+        System.out.println("Drag the mouse while holding the left mouse button to rotate the camera");
+        System.out.println("Press ENTER to change the center position");
+        System.out.println("Scroll the mouse-wheel to zoom in/out");
+        glfwSetKeyCallback(window, (win, k, s, a, m) -> {
+            if (k == GLFW_KEY_ESCAPE && a == GLFW_RELEASE)
+                glfwSetWindowShouldClose(window, true);
+            if (k == GLFW_KEY_ENTER && a == GLFW_PRESS) {
+                center.set((float) Math.random() * 20.0f - 10.0f, 0.0f, (float) Math.random() * 20.0f - 10.0f);
             }
-            cam.zoom(zoom);
-
-            /* Compute delta time */
-            long thisTime = System.nanoTime();
-            float diff = (float) ((thisTime - lastTime) / 1E9);
-            lastTime = thisTime;
-            /* And let the camera make its update */
-            cam.update(diff);
-
+        });
+        glfwSetFramebufferSizeCallback(window, (win, w, h) -> {
+            if (w > 0 && h > 0) {
+                width = w;
+                height = h;
+            }
+        });
+        glfwSetCursorPosCallback(window, (win, x, y) -> {
+            if (glfwGetMouseButton(win, GLFW_MOUSE_BUTTON_1) == GLFW_PRESS) {
+                yaw += ((int) x - mouseX) * 0.01f;
+                pitch += ((int) y - mouseY) * 0.01f;
+            }
+            mouseX = (int) x;
+            mouseY = (int) y;
+        });
+        glfwSetScrollCallback(window, (win, x, y) -> {
+            if (y > 0) {
+                zoom /= 1.1f;
+            } else {
+                zoom *= 1.1f;
+            }
+        });
+        IntBuffer framebufferSize = BufferUtils.createIntBuffer(2);
+        nglfwGetFramebufferSize(window, memAddress(framebufferSize), memAddress(framebufferSize) + 4);
+        width = framebufferSize.get(0);
+        height = framebufferSize.get(1);
+        glfwMakeContextCurrent(window);
+        GL.createCapabilities();
+        glClearColor(0.9f, 0.9f, 0.9f, 1.0f);
+        glEnable(GL_DEPTH_TEST);
+        Matrix4f mat = new Matrix4f();
+        FloatBuffer fb = BufferUtils.createFloatBuffer(16);
+        while (!glfwWindowShouldClose(window)) {
             glViewport(0, 0, width, height);
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-            mat.setPerspective((float) Math.atan((ViewSettings.screenHeight * height / ViewSettings.screenHeightPx) / ViewSettings.distanceToScreen),
-                               (float) width / height, 0.01f, 100.0f)
-               .get(fb);
             glMatrixMode(GL_PROJECTION);
-            glLoadMatrixf(fb);
-
-            /*
-             * Obtain the camera's view matrix and render grid.
-             */
-            cam.viewMatrix(mat.identity()).get(fb);
+            glLoadMatrixf(
+                    mat.setPerspective((float) Math.toRadians(45.0f), (float) width / height, 0.01f, 100.0f)
+                       .get(fb));
             glMatrixMode(GL_MODELVIEW);
-            glLoadMatrixf(fb);
+            // Load arcball camera view matrix into 'mat':
+            glLoadMatrixf(
+                    mat.translation(0, 0, -zoom)
+                       .rotateX(pitch)
+                       .rotateY(yaw)
+                       .translate(-center.x, -center.y, -center.z)
+                       .get(fb));
             renderGrid();
-
-            /* Translate to cube position and render cube */
-            mat.translate(cam.centerMover.target).get(fb);
+            // apply model transformation to 'mat':
+            mat.translate(center).get(fb);
             glLoadMatrixf(fb);
             renderCube();
-
             glfwSwapBuffers(window);
             glfwPollEvents();
         }
-    }
-
-    public static void main(String[] args) {
-        new ArcBallCameraDemo().run();
     }
 }
